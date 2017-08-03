435eaf394f511cb282fdfbde4ef6a5cf01da19fa
Balázs Czap <balazsczap@zooshgroup.com>
2017-08-03 12:43:36 +0200

    fixed tooltip export in index

--- spec/components/tooltip.js
+++ spec/components/tooltip.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tooltip from '../../components/tooltip';
+import { Tooltip } from '../../components';
 import { Button, Card } from 'react-toolbox';
 
 class Asd extends React.Component {
